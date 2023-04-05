77e6ae90a42480af0955eef857962b420a61fa5b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-05 20:07:01 +0000

    fix npx errors

--- utils/redis.js
+++ utils/redis.js
@@ -3,21 +3,21 @@ import { createClient } from 'redis';
 // A redis instance class for managing storage
 
 class RedisClient {
-
   constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log(err));
-    this.client.connect;
   }
 
   isAlive() {
     let alive = false;
-    this.client.on('connect', () => alive = true)
+    this.client.on('connect', () => {
+      alive = true;
+    });
     return alive;
   }
 
   async get(key) {
-    const val = await this.client.get(key)
+    const val = await this.client.get(key);
     return val;
   }
 
