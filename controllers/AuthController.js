c436a3d0e19a0713ee171186d91a6a331124984a
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-08 05:24:08 +0000

    check authentication errors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,19 +7,27 @@ import redisClient from '../utils/redis';
 class AuthController {
   static async getConnect(req, res) {
     const auth = req.headers.authorization.split(' ')[1];
-    const details = atob(auth).split(':');
-    const email = details[0];
-    const hash = crypto.createHash('sha1');
-    const pass = hash.update(details[1]).digest('hex');
-    if (await dbClient.findUser({ email, password: pass }) === 0) {
+    const decryptDetails = atob(auth);
+    console.log(decryptDetails);
+    console.log(decryptDetails.indexOf(':'))
+    if (decryptDetails.indexOf(':') === -1) {
       res.status(401).send({ error: 'Unauthorized' });
-    } else {
-      const token = uuid();
-      const key = `auth_${token}`;
-      await redisClient.set(key, email, 60 * 60 * 24);
-      res.send({
-        token,
-      });
+    }
+    else {
+      const details = decryptDetails.split(':');
+      const email = details[0];
+      const hash = crypto.createHash('sha1');
+      const pass = hash.update(details[1]).digest('hex');
+      if (await dbClient.findUser({ email: email, password: pass }) === 0) {
+        res.status(401).send({ error: 'Unauthorized' });
+      } else {
+        const token = uuid();
+        const key = `auth_${token}`;
+        await redisClient.set(key, email, 60 * 60 * 24);
+        res.send({
+          token,
+        });
+      }
     }
   }
 
