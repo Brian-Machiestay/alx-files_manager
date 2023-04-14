40a4bde9a07eef1c058ea983113066b2f9a237b9
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-14 13:53:21 +0000

    Revert "check status code of user object"
    This reverts commit 3d80039162d8c5d45f20cf5de16e61f8c6e466a4.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -19,7 +19,7 @@ class UsersController {
         password: hash.update(req.body.password).digest('hex'),
       };
       const { ops } = await dbClient.insertUser(ob);
-      res.status(201).send({
+      res.send({
         id: ops[0]._id,
         email: req.body.email,
       });
