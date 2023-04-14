467c1e8849b3f2352ce8ea2c9c60dc0eacee12b3
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-14 10:01:15 +0000

    set status 200

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,14 +3,14 @@ import dbClient from '../utils/db';
 
 class AppController {
   static async getStatus(req, res) {
-    res.send({
+    res.status(200).send({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
     });
   }
 
   static async getStats(req, res) {
-    res.send({
+    res.status(200).send({
       users: await dbClient.nbUsers(),
       files: await dbClient.nbFiles(),
     });
