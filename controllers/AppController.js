3cffb061273b502bb45148e352ff2efce7145621
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-14 17:09:55 +0000

    stringify response

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,17 +3,17 @@ import dbClient from '../utils/db';
 
 class AppController {
   static async getStatus(req, res) {
-    res.status(200).send({
+    res.status(200).send(JSON.stringify({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    });
+    }));
   }
 
   static async getStats(req, res) {
-    res.status(200).send({
+    res.status(200).send(JSON.stringify({
       users: await dbClient.nbUsers(),
       files: await dbClient.nbFiles(),
-    });
+    }));
   }
 }
 
