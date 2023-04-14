29acde1c23a038914ee0a69bd236b2fac15bdc4a
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-14 17:28:36 +0000

    change the status of file

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,17 +3,17 @@ import dbClient from '../utils/db';
 
 class AppController {
   static async getStatus(req, res) {
-    res.status(200).send(JSON.stringify({
+    res.status(200).send({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    }));
+    });
   }
 
   static async getStats(req, res) {
-    res.status(200).send(JSON.stringify({
+    res.status(200).send({
       users: await dbClient.nbUsers(),
       files: await dbClient.nbFiles(),
-    }));
+    });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
 
 const app = express();
 
@@ -13,22 +11,6 @@ app.get('/status', (req, res) => {
 
 app.get('/stats', (req, res) => {
   AppController.getStats(req, res);
-});
-
-app.post('/users', (req, res) => {
-  UsersController.postNew(req, res);
-});
-
-app.get('/connect', (req, res) => {
-  AuthController.getConnect(req, res);
-});
-
-app.get('/disconnect', (req, res) => {
-  AuthController.getDisconnect(req, res);
-});
-
-app.get('/users/me', (req, res) => {
-  UsersController.getMe(req, res);
 });
 
 export default app;
