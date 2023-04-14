609804178bf0fc69d3bc8744e0221c00da9f3cc7
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-14 18:02:16 +0000

    post a user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const app = express();
 
@@ -11,6 +12,10 @@ app.get('/status', (req, res) => {
 
 app.get('/stats', (req, res) => {
   AppController.getStats(req, res);
+});
+
+app.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
 });
 
 export default app;
